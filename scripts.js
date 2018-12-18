d4b97b587c017d7919ce2a6dcdf5bb0d710d2297
DigitalCrafts <digitalcrafts@DigitalCraftss-MacBook-Air.local>
2018-12-18 14:05:06 -0500

    Updated Search Bar

--- scripts.js
+++ scripts.js
@@ -8,26 +8,34 @@ $(document).ready(()=>{
         console.log(event);
 
         const symbol = $('#symbol').val();
+        $('#symbol').val('');
         console.log(symbol);
-        
-        const url = `https://api.iextrading.com/1.0/stock/${symbol}/quote`
-             // getJSON method takes 2 args:
+
+        const symbols = symbol.split(',')
+
+        symbols.forEach((s)=>{
+            s = s.trim();
+            
+
+            const url = `https://api.iextrading.com/1.0/stock/${s}/quote`
+            // getJSON method takes 2 args:
             // 1. where to get JSON
             // 2. function to run when Im back
-        $.getJSON(url,(theDataJSFoundIfAny)=>{
-         console.log(theDataJSFoundIfAny)
-            $('#stock-body').html(`
-        <tr>
-            <td>${theDataJSFoundIfAny.symbol}</td>
-            <td>${theDataJSFoundIfAny.companyName}</td>
-            <td>${theDataJSFoundIfAny.high}</td>
-            <td>${theDataJSFoundIfAny.low}</td>
-            <td>${theDataJSFoundIfAny.change}</td>
-         </tr>
-         
-           `)
+            $.getJSON(url,(theDataJSFoundIfAny)=>{
+                 console.log(theDataJSFoundIfAny)
+                $('#stock-body').append(`
+            <tr>
+                <td>${theDataJSFoundIfAny.symbol}</td>
+                <td>${theDataJSFoundIfAny.companyName}</td>
+                <td>${theDataJSFoundIfAny.high}</td>
+                <td>${theDataJSFoundIfAny.low}</td>
+                <td>${theDataJSFoundIfAny.change}</td>
+            </tr>
+            
+            `)
+            })
+        
         })
-    
-    })
+    });
 });
 
